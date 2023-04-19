ba0478962ce17761d6ef4fdc01988ecd57cdcf9f
Ivan Pozdov <Ivan.pozdov58@gmail.com>
2023-04-19 22:56:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int mile = 20;  // за столько рублей будет даваться 1 миля.
+
+        int price = 6568; // Стоимость билета.
+        int bonus = price / mile;
+        System.out.println("Количество бонусов=" + bonus);
+
+
+    }
+
+}
